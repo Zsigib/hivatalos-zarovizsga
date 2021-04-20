a635b04ac5fe73ba4596274e2b3c9476e8b6e092
Zsigi <zsigib@freemail.hu>
2021-04-20 11:52:05 +0200

    Lejárt az idő!

--- src/main/java/hu/nive/ujratervezes/zarovizsga/housecup/HouseCup.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/housecup/HouseCup.java
@@ -2,12 +2,26 @@ package hu.nive.ujratervezes.zarovizsga.housecup;
 
 import org.mariadb.jdbc.MariaDbDataSource;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class HouseCup {
 
+    private DataSource dataSource;
+
     public HouseCup(MariaDbDataSource dataSource) {
+            this.dataSource = dataSource;
     }
 
-    public int getPointsOfHouse(String gryffindor) {
-    return 0;}
+    public int getPointsOfHouse(String house) {
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement("");
+
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect");
+        }
+        return 0;}
 
 }
